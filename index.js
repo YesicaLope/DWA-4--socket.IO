8af608d03a135d4dbbcce56e7952d3fa567da927
YesicaLope <yesica.lope@tecsup.edu.pe>
2022-04-09 12:45:13 -0500

    Crear nuestro servidor- Paso 3

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 import express from "express";
 
-//ESTE FROMA NO ES IDEAL // const { Server } = require("socket.io");
+//PASO 3--> CREAR NUESTRO SERVIDOR
+const express = require('express')
+const app = express();
+
+const http = require('http')
+const server = http.createServer(app)
+
+//PASO 2 -> AGREGAR SOCKET.IO
+//ESTE FROMA NO ES IDEAL-> const { Server } = require("socket.io");
 //La correcta en la siguiente linea
 import { Server } from "socket.io";
 const io = new Server(server)
 
 
-const app = express();
+
+
+
+
+
+
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
